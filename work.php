2b0f096ad4500b1a7bb6cd1f5ebaedf08a8ffd00
Simon Nonnis <simononnis@gmail.com>
2016-02-08 13:20:54 +0100

    Update work page

--- work.php
+++ work.php
@@ -7,37 +7,18 @@
         <img alt="different-devices" src="img/work/work.png" />
       </div>
       <h2>Recent Work</h2>
-      <article class="work-article wrapper clearfix artistresidence">
-        <div class="work-txt">
-          <h3><a href="http://www.artistresidence.co.uk/" target="_blank">artistresidence</a></h3>
-          <p>An eccentric bunch of fun and friendly hotels in London, Brighton and Cornwall.
-          With each room designed by a different artist, it's an utterly charming place to stay.
-          </p>
-        </div>
-      </article>
-      <article class="work-article wrapper clearfix bluefin">
-        <div class="work-txt">
-          <h3><a href="http://www.bluefinnyc.com/" target="_blank">blue fin</a></h3>
-          <p>A modern seafood restaurant, where Broadway energy meets quiet sophistication.
-             Located in the heart of Times Square, Blue Fin is sophisticated and sexy, and offers the freshest sushi, seafood and raw bar.
-          </p>
-        </div>
-      </article>
-      <article class="work-article wrapper clearfix theclaremont">
+
+
+
+
+
+
+      <!-- <article class="work-article wrapper clearfix theclaremont">
         <div class="work-txt">
           <h3><a href="http://dev.claremonthoteldouglas.co.uk/" target="_blank">the claremont</a></h3>
-          <p>Designed For The Business Traveller. 56 luxury en-suite bedrooms, 
-             including a range of executive rooms with stunning views of the Isle of Man’s coastline, 
+          <p>Designed For The Business Traveller. 56 luxury en-suite bedrooms,
+             including a range of executive rooms with stunning views of the Isle of Man’s coastline,
              make The Claremont your ideal business base.
-          </p>
-        </div>
-      </article>
-      <article class="work-article wrapper clearfix gliffaeshotel">
-        <div class="work-txt">
-          <h3><a href="http://www.gliffaeshotel.com/" target="_blank">gliffaeshotel</a></h3>
-          <p>In the heart of the Brecon Beacons National Park, One of the last real fishing hotels, 
-             this relaxing haven is located off the beaten track in 33 acres of stunning grounds, 
-             surrounded by magnificent trees and next to the River Usk.
           </p>
         </div>
       </article>
@@ -57,20 +38,46 @@
              horizontal menu or open the left-side menu swiping with your finger's tip.
           </p>
         </div>
-      </article>
-      <article class="work-article wrapper clearfix bio-chem">
+      </article> -->
+
+
+
+      <article class="work-article wrapper clearfix artistresidence">
         <div class="work-txt">
-          <h3><a href="demo/bio-chem/home" target="_blank">bio-chem</a></h3>
-          <p>Bio-Chem Laboratory provides high quality analysis services with outstanding customer service.
-             Bio-Chem employs highly experienced chemists and uses an arsenal of the latest technology available
-             to serve its customers. Bio-Chem Laboratory offers common analysis services using state-of-the-art equipment
-             and highly trained and qualified chemists. Bio-Chem Laboratory performs analysis of soil, water or air filters.
+          <h3><a href="http://www.artistresidence.co.uk/" target="_blank">Artistresidence</a></h3>
+          <p>An eccentric bunch of fun and friendly hotels in London, Brighton and Cornwall.
+          With each room designed by a different artist, it's an utterly charming place to stay.
+          </p>
+        </div>
+      </article>
+      <article class="work-article wrapper clearfix chefs-club">
+        <div class="work-txt">
+          <h3><a href="http://www.chefsclub.com/new-york/" target="_blank">Chefs Club</a></h3>
+          <p>Chefs Club New York is a brand new restaurant in downtown Manhattan,
+            serving inventive recipes from Food &amp; Wine's Best New Chefs.
+          </p>
+        </div>
+      </article>
+      <article class="work-article wrapper clearfix bluefin">
+        <div class="work-txt">
+          <h3><a href="http://www.bluefinnyc.com/" target="_blank">Blue Fin</a></h3>
+          <p>A modern seafood restaurant, where Broadway energy meets quiet sophistication.
+             Located in the heart of Times Square, Blue Fin is sophisticated and sexy, and offers the freshest sushi, seafood and raw bar.
+          </p>
+        </div>
+      </article>
+      <article class="work-article wrapper clearfix gliffaeshotel">
+        <div class="work-txt">
+          <h3><a href="http://www.gliffaeshotel.com/" target="_blank">Gliffaes Hotel</a></h3>
+          <p>In the heart of the Brecon Beacons National Park, One of the last real fishing hotels,
+             this relaxing haven is located off the beaten track in 33 acres of stunning grounds,
+             surrounded by magnificent trees and next to the River Usk.
           </p>
         </div>
       </article>
       <article class="work-article wrapper clearfix emilyhankins">
         <div class="work-txt">
-          <h3><a href="http://emilyhankins.co.uk/" target="_blank">emilyhankins.co.uk</a></h3>
+          <h3><a href="http://emilyhankins.co.uk/" target="_blank">Emily Hankins</a></h3>
           <p>Emily Hankins is a Cornish based artist and designer with a passion for cake and a love
              of all things vintage and handmade. Seeing an increased demand for individual and bespoke
              weddings with a vintage, homemade and eclectic feel she decided to launch her own business
