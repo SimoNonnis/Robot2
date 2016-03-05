e7ae1dad979f42535b03b8ba3e251ea00aae1cb8
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2016-03-05 11:41:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,12 @@
 
     <section role="main" class="home-main-section wrapper clearfix">
       <article class="home-main-article clearfix">
-        <h2>high performance website development</h2>
-        <p>If you think your business deserves a super fast, hand coded, innovative website.
-           Stand out from the crowd. Do not look longer outside. You are in the right place!
+        <h2>Keep it extremely simple</h2>
+        <p>Develop for users, clients, teammates, your future self. 
+           You should strive not just writing code that works correctly, 
+           but code that make sense when examined, easy to read and refactor. 
+           Keep it extremely simple, 'cause code is mostly for humans, computers 
+           only cares about machine code.
         </p>
       </article>
     </section>
