0ff2a76ec602bece3c712bd9bd80a4d3313b3a06
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2016-03-08 11:19:22 +0100

    Update work.php

--- work.php
+++ work.php
@@ -11,7 +11,7 @@
         Here in this section I like to share a list of my recent projects I’ve worked on and that I am proud of.
         Each of these websites represents an important step of my programming philosophy.
         My part in every project consisted of interacting with graphic designers and project managers designing
-        information architecture, UX, functionalities, setting up infrastructures, building from scratch responsive websites.
+        information architecture, UX, functionalities and at the same time setting up infrastructures, building from scratch these responsive websites.
       </p>
 
 
