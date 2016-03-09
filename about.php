0509cd44f1c7a7d75e96f1c5499cf066cb02e398
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2016-03-09 11:10:02 +0100

    Update about.php

--- about.php
+++ about.php
@@ -22,10 +22,10 @@
           </p>
           
           <p>I gained an extensive experience on simplifying the process to collaborate 
-             with UX designers and project managers to deliever stable, fast and high quality websites.
+             with UX designers and project managers to deliver stable, fast and high quality websites.
           </p>
              
-          <p>Below you can see a short list of languages, tools, software that I love to work with.</p>   
+          <p>Below you can see a short list of languages, tools, softwares that I love to work with.</p>   
         </div>
   
         <div class="focused focused-code clearfix">
@@ -57,13 +57,12 @@
         <div class="wrapper content">
           <p>In mid 2015 I felt the need to take a break and dedicate myself to learn Javascript in deep.
              My goal is to be able to join a team where Javascript is at the forefront on a daily basis.
-             I’m by no means at an expert level and I’m still learning lots. 
-             When it comes to coding no one is the best and if you really enjoy it you’ll always keep progressing.
+             I am not at an expert level but when it comes to coding if you really enjoy it you’ll always keep progressing.
           </p>
           <p>"You don’t know JS" book series by 
              <a href="https://twitter.com/getify" target="_blank">Kyle Simpson</a> 
-             are a big part into learning relevant information about JavaScript. 
-             All the books are on <a href="https://github.com/getify/You-Dont-Know-JS" target="_blank">Github</a> and free to view! 
+             are a fundamental resource into my learning process. 
+             All the books are free to read on <a href="https://github.com/getify/You-Dont-Know-JS" target="_blank">Github</a>!  
              I’ve bought the books <a href="http://www.amazon.com/s/ref=nb_sb_ss_c_0_18?url=search-alias%3Dstripbooks&amp;field-keywords=you+don%27t+know+javascript&amp;sprefix=you+don%27t+know+javascript%2Caps%2C204" target="_blank">hardcopies</a>
              so I have them for offline reading and more importantly I was able to support Kyle with his good work.
           </p>
