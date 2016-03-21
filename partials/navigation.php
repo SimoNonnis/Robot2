d769a54568ae5fbf1fc0075a67c91d826438b566
Simon Nonnis <simononnis@gmail.com>
2016-03-21 13:41:24 +0100

    Update on scss folder

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,5 +1,5 @@
-<header role="banner" class="header">
-<div class="header-container u-cf">
+<header role="banner" class="glb-header">
+<div class="glb-header-container u-cf">
   <div class="logo">
     <h1><a href="/">Robot2</a></h1>
     <p>making programming simpler</p>
