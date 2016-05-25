a0eacfee45cdbac953411e200b047b166e9ecefc
Simon Nonnis <simononnis@gmail.com>
2016-05-25 13:57:34 +0200

    Add h3 to about page paragraphs

--- about.php
+++ about.php
@@ -61,6 +61,8 @@
           </div>
         </div>
         <div class="g-wrapper content">
+
+          <h3 class="h3">Javascript</h3>
           <p>In mid 2015 I felt the need to take a break and dedicate myself to learn Javascript in deep.
              My goal is to be able to join a team where Javascript is at the forefront on a daily basis.
           </p>
@@ -71,6 +73,11 @@
              I’ve bought the books <a href="http://www.amazon.com/s/ref=nb_sb_ss_c_0_18?url=search-alias%3Dstripbooks&amp;field-keywords=you+don%27t+know+javascript&amp;sprefix=you+don%27t+know+javascript%2Caps%2C204" target="_blank">hardcopies</a>,
              because frequently I need to go back to some chapter and concept, so I have them for offline reading.
           </p>
+          <p>I am a big fan of <a href="https://github.com/getify/You-Dont-Know-JS/blob/master/this%20%26%20object%20prototypes/ch6.md" target="_blank">OLOO (objects linking to other objects)</a> so I agree with Eric Elliot
+             when he advocates against the use of <a href="https://vimeo.com/69255635" target="_blank">class pattern</a> in js.
+          </p>
+
+          <h3 class="h3">React & Frameworks</h3>
           <p>During this time, I had a look at js frameworks like Angular, Ember or Meteor,
              and I’ve come to realise that frameworks increase your project's complexity, dependencies and limit your scope of creativity.
              I prefer core-language solutions, small libraries and micro frameworks, with small API surface, that are close as possible to vanilla Js.
@@ -81,9 +88,8 @@
              <a href="http://elm-lang.org/"  target="_blank">Elm</a>.
           </p>
           <p>For the most part best practices of React, for example, are just JavaScript best practices.</p>
-          <p>I am a big fan of <a href="https://github.com/getify/You-Dont-Know-JS/blob/master/this%20%26%20object%20prototypes/ch6.md" target="_blank">OLOO (objects linking to other objects)</a> so I agree with Eric Elliot
-             when he advocates against the use of <a href="https://vimeo.com/69255635" target="_blank">class pattern</a> in js.
-          </p>
+
+          <h3 class="h3">Functional Programming</h3>
           <p>These days I took a shine to functional programming that opens up a different style of programming.
              Key concept in FP is split the logic from side effects like: network calls, files read or written, database queries, modified DOM elements or global variables.
              The trouble with this is that the whole point of writing computer programs is the side effects. Making a network call and rendering DOM elements is at the core
@@ -96,6 +102,8 @@
              Keep functions small. Do one thing at a time, and do it well.
           </p>
           <p>I pursuit the idea of simpler programming, FP helps me to go in the right direcion.</p>
+
+          <h3 class="h3">Figuring things out</h3>
           <p>At the end though Web development is not all that much about coding, but often
              is about of figuring things out, getting in a mood to solving a small problem after the other.
              Sometime is coded related sometime is just doing research about tooling, sometime you get stucked and your
