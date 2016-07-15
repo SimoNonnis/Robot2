1f18a20e90db80776eec81242c57ef86bbd1e7ff
Simon Nonnis <simononnis@gmail.com>
2016-07-15 17:21:40 +0200

    'Add new logo svg'

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,8 +1,13 @@
 <header role="banner" class="g-header">
 <div class="g-header-container u-cf">
   <div class="logo">
-    <h1><a class="u-noTextDecoration" href="/">Robot2</a></h1>
-    <p>making programming simpler</p>
+    <a class="u-noTextDecoration u-block" href="/">
+      <img class="logo__svg" src="dist/images/svg/robot2.svg" alt="Robot2">
+    </a>
+    <div class="logo__div">
+      <h1><a class="u-noTextDecoration logo__title" href="/">Robot2</a></h1>
+      <p class="logo__description">making programming simpler</p>
+    </div>
   </div>
 
   <nav role="navigation" class="main-nav">
--- src/images/svg/README.md
+++
@@ -1,6 +0,0 @@
-In Git, you cannot commit empty folders, because Git does not actually save folders, only files.
-More info:
-  http://stackoverflow.com/questions/14541253/commit-empty-folder-structure-with-git
-  http://stackoverflow.com/questions/7229885/what-are-the-differences-between-gitignore-and-gitkeep
-
-You can just delete this file when starting to add yours.
