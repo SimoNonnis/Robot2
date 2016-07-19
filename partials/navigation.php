9ce390577393d22c8acd2ac63db4fb86cdcde0aa
Simon Nonnis <simononnis@gmail.com>
2016-07-19 11:24:12 +0200

    'Removed robot2 near logo'

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,12 +1,11 @@
 <header role="banner" class="g-header">
 <div class="g-header-container u-cf">
   <div class="logo">
-    <a class="u-noTextDecoration u-block" href="/">
-      <img class="logo__svg" src="dist/images/svg/robot2.svg" alt="Robot2">
-    </a>
-    <div class="logo__div">
-      <h1><a class="u-noTextDecoration logo__title" href="/">Robot2</a></h1>
-    </div>
+    <h1>
+      <a class="u-noTextDecoration u-block" href="/">
+        <img class="logo__svg" src="dist/images/svg/robot2.svg" alt="Robot2">
+      </a>
+    </h1>
   </div>
 
   <nav role="navigation" class="main-nav">
