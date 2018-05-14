60b71c232d113661e9cfc180127e65f7498a8a50
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2018-05-14 20:18:29 +0100

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -9,7 +9,6 @@
   </nav>
 
   <div class="social-links">
-  	<a href="https://twitter.com/SimoNonnis" target="_blank"><strong>twitter</strong></a>
   	<a href="http://codepen.io/SimonNonnis/" target="_blank"><strong>codepen</strong></a>
   	<a href="https://github.com/SimoNonnis" target="_blank"><strong>github</strong></a>
   </div>
