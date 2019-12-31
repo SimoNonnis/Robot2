a922d3f3cf3986e8f69471d7968722b389f8725a
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2019-12-31 11:27:23 +0000

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,19 +1,20 @@
 <footer role="contentinfo" class="footer">
-<div class="g-wrapper footer-container u-cf">
-  <p class="footer-email">
-    <strong>email:</strong>
-    <span>simon.nonnis@protonmail.com</span>
-  </p>
-  <nav class="footer-nav">
-    <?php include("nav-list.php"); ?>
-  </nav>
+  <div class="g-wrapper footer-container u-cf">
+    <p class="footer-email">
+      <strong>email:</strong>
+      <span>simon.nonnis@protonmail.com</span>
+    </p>
+    
+    <nav class="footer-nav">
+      <?php include("nav-list.php"); ?>
+    </nav>
 
-  <div class="social-links">
-  	<a href="https://github.com/SimoNonnis" target="_blank"><strong>github</strong></a>
+    <div class="social-links">
+      <a href="https://github.com/SimoNonnis" target="_blank"><strong>github</strong></a>
+    </div>
+
+    <p class="copy">Robot2&nbsp;&copy;&nbsp;<?php echo date("Y") ?></p>
   </div>
-
-  <p class="copy">Robot2&nbsp;&copy;&nbsp;<?php echo date("Y") ?></p>
-</div>
 </footer>
 
 </body>
