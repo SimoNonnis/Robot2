68d5075baf58904013d8d0053915cc2cb0e98f22
Simon Nonnis <simon.nonnis@protonmail.com>
2023-04-22 00:45:55 +0300

    Silence warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{html}"],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
