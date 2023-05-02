cf495b4445a12a695aed6d73621a0347b215422a
Simon Nonnis <simon.nonnis@protonmail.com>
2023-05-02 09:59:28 +0300

    Remove OLD folder

--- OLD/partials/get-in-touch.php
+++
@@ -1,4 +0,0 @@
-
-<div class="g-wrapper-cta u-cf">
-	<a href="contact-me" class="u-noTextDecoration">get in touch</a>
-</div>
--- OLD/partials/nav-list.php
+++
@@ -1,7 +0,0 @@
-
-<ul class="g-listClean nav-list u-cf">
-  <li class="home-a"><a href="/">home</a></li>
-  <li class="about-a"><a href="about">about</a></li>
-  <li class="work-a"><a href="work">work</a></li>
-  <li class="contact-a"><a href="contact-me">contact</a></li>
-</ul>
